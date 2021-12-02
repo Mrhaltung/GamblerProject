a1699265f05e8fccb3590200d7004c411b583b40
Mrhaltung <dipeshchandekar99@gmail.com>
2021-12-02 18:29:38 +0530

    Add project files.

---
+++ GamblerProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GamblerProject
+{
+    class Program
+    {
+        public int start = 100;
+        public int bet = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gambler Progarm \n");
+           
+        }
+    }
+}
